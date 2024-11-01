f9e9d388bfb665f515ef94608f7c6de571934b81
Josh Fryer <josh.s.fryer@gmail.com>
2024-11-01 14:48:04 -0400

    Add some more colorschemes to switch between

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,11 @@
 return {
-  --[[
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
-    ]]
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-mocha",
+  --   },
+  -- },
 
   {
     "folke/tokyonight.nvim",
@@ -15,4 +13,26 @@ return {
     priority = 1000,
     opts = { style = "storm" },
   },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  --
+  -- { "savq/melange-nvim", lazy = false, priority = 1000 },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "melange",
+  --   },
+  -- },
+
+  -- { "comfysage/gruvboxed", lazy = false, priority = 1000, opts = { contrast_dark = "soft" } },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvboxed",
+  --   },
+  -- },
 }
